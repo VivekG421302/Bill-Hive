b3498de0f65ff87a1ea2b019c3e61f1873590a56
VIVEK GUPTA <110884990+VivekG421302@users.noreply.github.com>
2026-07-11 10:04:20 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // ===================== BILL-HIVE SERVICE WORKER v2.2 =====================
-const CACHE_NAME = 'billhive-v4.02';
+const CACHE_NAME = 'billhive-v4.03';
 const STATIC_ASSETS = [
   '/',
   '/index.html',
